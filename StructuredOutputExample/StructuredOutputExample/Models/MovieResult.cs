166d9563259a7a12b33eaf8475591a29ced2e13d
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2025-02-12 18:25:43 +0100

    WIP

--- StructuredOutputExample/StructuredOutputExample/Models/MovieResult.cs
+++ StructuredOutputExample/StructuredOutputExample/Models/MovieResult.cs
@@ -5,12 +5,14 @@ namespace StructuredOutputExample.Models;
 public class MovieResult
 {
     public required string MessageBack { get; set; }
-
-    //[Description("The average score each movie have in the Top10Movies")]
-    //public required decimal AverageScoreOfTheMovies { get; set; }
+    
+    public required decimal AverageScoreOfThe10Movies { get; set; } //Example that we still deal with "stupid AIs" but also that such things are not really needed
 
     [Description("Order them by IMDB Score")] //Sometimes the Structured output properties need a bit of description to get desired effect
     public required Movie[] Top10Movies { get; set; }
 
-    //NB: Please note that structured output do not support DateTime/DateTimeOffset types :-( ... Instead, get it as string and used description to inform AI of date format. Then parse it manually
+    public decimal RealAverageScore => Top10Movies.Select(x => x.ImdbScore).Average();
+
+    //NB: Please note that structured output are poor with DateTime/DateTimeOffset types at the moment [Need custom instructions to be able to parse]
+    //https://github.com/microsoft/semantic-kernel/issues/10507
 }
--- StructuredOutputExample/StructuredOutputExample/Program.cs
+++ StructuredOutputExample/StructuredOutputExample/Program.cs
@@ -1,5 +1,5 @@
 ﻿using StructuredOutputExample;
 
-await NoStructuredOutput.Run(); //Note that you format is not 100% the same when you ask. and if you want specific/more info you need to write quite a big instructions prompt
+//await NoStructuredOutput.Run(); //Note that you format is not 100% the same when you ask. and if you want specific/more info you need to write quite a big instructions prompt
 
-//await StructuredOutputExample.StructuredOutputExample.Run(); //Structured output makes that easier :-)
+await StructuredOutputExample.StructuredOutputExample.Run(); //Structured output makes that easier :-)
