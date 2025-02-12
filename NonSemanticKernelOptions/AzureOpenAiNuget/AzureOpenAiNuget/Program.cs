7ed76b56cff072de16ed416141b057088dcfae51
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-02-12 09:27:35 +0100

    Added AzureOpenAI and OpenAi raw Nuget Access

---
+++ NonSemanticKernelOptions/AzureOpenAiNuget/AzureOpenAiNuget/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string azureOpenAiEndpoint = config["AiEndpoint"]!;
+string azureOpenAiKey = config["AiKey"]!;
+const string chatModel = "gpt-4o-mini";
+
+
+var client = new AzureOpenAIClient(new Uri(azureOpenAiEndpoint), new AzureKeyCredential(azureOpenAiKey));
+ChatClient chatClient = client.GetChatClient(chatModel);
+
+List<ChatMessage> messages = [];
+while (true)
+{
+    Console.Write("> ");
+    var inputFromUser = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(inputFromUser))
+    {
+        messages.Add(new UserChatMessage(inputFromUser));
+        await foreach (var response in chatClient.CompleteChatStreamingAsync(messages))
+        {
+            if (response.ContentUpdate.Count > 0)
+            {
+                Console.Write(response.ContentUpdate[0].Text);
+            }
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(string.Empty.PadLeft(50, '*'));
+    Console.WriteLine();
+}
---
+++ NonSemanticKernelOptions/OpenAiNuget/OpenAiNuget/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI;
+using OpenAI.Chat;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+string openAiKey = config["AiKey"]!;
+const string chatModel = "gpt-4o-mini";
+
+
+var client = new OpenAIClient(openAiKey);
+ChatClient chatClient = client.GetChatClient(chatModel);
+
+List<ChatMessage> messages = [];
+while (true)
+{
+    Console.Write("> ");
+    var inputFromUser = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(inputFromUser))
+    {
+        messages.Add(new UserChatMessage(inputFromUser));
+        await foreach (var response in chatClient.CompleteChatStreamingAsync(messages))
+        {
+            if (response.ContentUpdate.Count > 0)
+            {
+                Console.Write(response.ContentUpdate[0].Text);
+            }
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(string.Empty.PadLeft(50, '*'));
+    Console.WriteLine();
+}
