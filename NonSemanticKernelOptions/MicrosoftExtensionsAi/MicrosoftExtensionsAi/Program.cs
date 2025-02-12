3e27db61188633c70de749126df1c84f7255eedc
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2025-02-12 17:10:55 +0100

    WIP

--- NonSemanticKernelOptions/MicrosoftExtensionsAi/MicrosoftExtensionsAi/Program.cs
+++ NonSemanticKernelOptions/MicrosoftExtensionsAi/MicrosoftExtensionsAi/Program.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.Extensions.Configuration;
-using OpenAI;
-using OpenAI.Chat;
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
 
 var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-string openAiKey = config["AiKey"]!;
+string azureOpenAiEndpoint = config["AiEndpoint"]!;
+string azureOpenAiKey = config["AiKey"]!;
 const string chatModel = "gpt-4o-mini";
 
+//https://github.com/dotnet/extensions/tree/main/src/Libraries/Microsoft.Extensions.AI.OpenAI
+IChatClient chatClient = new ChatClientBuilder(
+        new AzureOpenAIClient(new Uri(azureOpenAiEndpoint), new AzureKeyCredential(azureOpenAiKey)).AsChatClient(modelId: chatModel))
+    .UseFunctionInvocation()
+.Build();
 
-var client = new OpenAIClient(openAiKey);
-ChatClient chatClient = client.GetChatClient(chatModel);
+ChatOptions chatOptions = new()
+{
+    //ResponseFormat = //Gave up after 30 min of trying to find a simple mode without defining it as string :-( SK is much better!!! 
+    Tools = [AIFunctionFactory.Create(GetWeather)]
+};
 
 List<ChatMessage> messages = [];
 while (true)
@@ -17,17 +33,23 @@ while (true)
     var inputFromUser = Console.ReadLine();
     if (!string.IsNullOrWhiteSpace(inputFromUser))
     {
-        messages.Add(new UserChatMessage(inputFromUser));
-        await foreach (var response in chatClient.CompleteChatStreamingAsync(messages))
+        messages.Add(new ChatMessage(ChatRole.User, inputFromUser));
+        await foreach (var response in chatClient.CompleteStreamingAsync(messages, chatOptions))
         {
-            if (response.ContentUpdate.Count > 0)
-            {
-                Console.Write(response.ContentUpdate[0].Text);
-            }
+            Console.Write(response.Text);
         }
     }
+    messages.Clear();
 
     Console.WriteLine();
     Console.WriteLine(string.Empty.PadLeft(50, '*'));
     Console.WriteLine();
+}
+
+[Description("Gets the weather")]
+static string GetWeather() => Random.Shared.NextDouble() > 0.5 ? "It's sunny" : "It's raining";
+public class MyClass
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
